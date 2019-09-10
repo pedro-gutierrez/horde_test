e2071665761a6c115dcf3cdda4b3526813ad553b
Pedro Gutiérrez <pedrogutierrez@mac.com>
2019-09-10 11:56:03 +0200

    fqdn on prod nodes

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ import Config
 
 config :horde_test,
     nodes: [
-        :node@a0,
-        :node@a1,
-        :node@a2
+        :"node@a0.public",
+        :"node@a1.public",
+        :"node@a2.public"
     ]
